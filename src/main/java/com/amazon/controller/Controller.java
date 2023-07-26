871519e28e67fa88bf6fde7c45736ff2cd16b9d8
Marvin Vasquez <wmvasq1@gmail.com>
2023-07-26 09:42:43 -0400

    cors config added

--- src/main/java/com/amazon/controller/Controller.java
+++ src/main/java/com/amazon/controller/Controller.java
@@ -3,6 +3,7 @@ package com.amazon.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.amazon.entity.Categoria;
 import com.amazon.repository.CategoriaRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 @RequestMapping("/api")
 public class Controller {
 
