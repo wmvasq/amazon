3d6552d18c15e6848da5b74564d06f3e02689d0f
Marvin Vasquez <wmvasq1@gmail.com>
2023-07-26 03:03:01 -0400

    endpoint categorias

---
+++ src/main/java/com/amazon/controller/Controller.java
@@ -0,0 +1,29 @@
+package com.amazon.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.amazon.entity.Categoria;
+import com.amazon.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+
+    @GetMapping("/saludo")
+    public String saludo(){
+        return "hola mundo";
+    }
+        @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping("/categorias")
+    public List<Categoria> getUsers() {
+        return categoriaRepository.findAll();
+    }
+}
---
+++ src/main/java/com/amazon/entity/Categoria.java
@@ -0,0 +1,27 @@
+package com.amazon.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categorias")    
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    public Categoria() {
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
